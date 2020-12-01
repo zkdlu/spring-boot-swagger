41794dff44015e4f4bcd6a1c90286423d69eb2cb
zkdlu <zkdlu159951@gmail.com>
2020-12-01 22:50:36 +0900

    first

---
+++ src/main/java/com/zkdlu/swagger/SwaggerController.java
@@ -0,0 +1,12 @@
+package com.zkdlu.swagger;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SwaggerController {
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
